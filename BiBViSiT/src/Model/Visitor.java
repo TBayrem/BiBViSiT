7673f9790c36907a93c14b0dec49e72d7bc7fc86
Bayrem Trabelsi <bayrem.trabelsi@outlook.com>
2021-02-24 21:01:51 +0100

    Update Visitor.java
    Model Visitors Created

--- BiBViSiT/src/Model/Visitor.java
+++ BiBViSiT/src/Model/Visitor.java
@@ -2,4 +2,45 @@ package Model;
 
 public class Visitor {
 
+	//here
+	private String ID;// Serial back to createtables
+	private String Psydo;
+	private String String;
+
+	public Visitor() {
+
+	}
+
+	public Visitor(java.lang.String iD, java.lang.String psydo, java.lang.String string) {
+		ID = iD;
+		Psydo = psydo;
+		String = string;
+	}
+
+	
+	
+	
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public String getPsydo() {
+		return Psydo;
+	}
+
+	public void setPsydo(String psydo) {
+		Psydo = psydo;
+	}
+
+	public String getString() {
+		return String;
+	}
+
+	public void setString(String string) {
+		String = string;
+	}
 }
