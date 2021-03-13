5c3e8d1c9c99f9cf804497145bc782bf68e45e13
Bayrem Trabelsi <bayrem.trabelsi@outlook.com>
2021-03-14 00:06:50 +0100

    Update VisitorView.js

--- BiBViSiT/WebContent/View/Template/js/VisitorView.js
+++ BiBViSiT/WebContent/View/Template/js/VisitorView.js
@@ -1,13 +1,13 @@
-var freePlaces = document.querySelector('.FreePlaces');
-var aktuplaces = localStorage.getItem('AFP')
-var aktuplacesInt = +localStorage.getItem('AFP')
+//var freePlaces = document.querySelector('.FreePlaces');
+//var aktuplaces = localStorage.getItem('AFP')
+//var aktuplacesInt = +localStorage.getItem('AFP')
 
-
-function myVisitor(){
-
+window.setInterval("refreshIt()",100);
+function refreshIt(){
+ var aktuplaces = localStorage.getItem('AFP')
  document.getElementById("texti").innerHTML = aktuplaces; 
  //freePlaces.textContent = aktuplaces;
- setTimeout(()=> {window.location.reload(true);}, 5000);
+ //setTimeout(()=> {window.location.reload(true);}, 60000);
     
  
 }
